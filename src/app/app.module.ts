8681f872e8f5b7952d14e4cfc6396c8e317fb9e0
Miguel Salinas <eliassid2@gmail.com>
2023-06-21 12:25:42 -0300

    se agrega route y layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { RoutesModule } from './components/routes/routes.module';
+import { LayoutModule } from './components/layout/layout.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutesModule,
+    LayoutModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
