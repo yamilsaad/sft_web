9ecd304ca48b05e57e56a1ff6e783ce0901ce4a7
Miguel Salinas <eliassid2@gmail.com>
2023-06-25 13:16:27 -0300

    se agrega seccion de nosotros

--- src/app/components/shared/shared.module.ts
+++ src/app/components/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonArrowComponent } from './button-arrow/button-arrow.component';
+import { ProfilesComponent } from './profiles/profiles.component';
 
 
 
 @NgModule({
-  declarations: [ButtonArrowComponent],
-  exports: [ButtonArrowComponent],
+  declarations: [ButtonArrowComponent, ProfilesComponent],
+  exports: [ButtonArrowComponent, ProfilesComponent],
   imports: [CommonModule]
 })
 export class SharedModule { }
